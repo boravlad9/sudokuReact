1f1901867f6807d2321e96ea89e01f6590dcfe81
boravlad9 <boravlad9@gmail.com>
2021-09-02 17:01:06 +0300

    login

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import {apiLogin} from '../Services/LoginService';
+import {
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 class Login extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {email : "", parola : ""};
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangeParola = this.handleChangeParola.bind(this);
+  }
+
+  loginCall(){
+    apiLogin(this.state.email, this.state.parola)
+    .then(response => console.log(response));
+  }
+
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handleChangeParola(event) {
+    this.setState({parola: event.target.value});
+  }
+
   render() {
     return (
-      <div>
-        <p>Ceva text</p>
+      <div style = {{padding : "10px"}}>
+        <label> Email</label>
+        <input value={this.state.email} onChange={this.handleChangeEmail}/>
+        <label> Parola</label>
+        <input type = "password" value={this.state.parola} onChange={this.handleChangeParola}/>
+        <button onClick={this.loginCall.bind(this)}>Login</button>
       </div>
     );
   }
--- src/Services/LoginService.js
+++ src/Services/LoginService.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+
+export const apiLogin = (email, parola) =>{
+//  axios.post('https://reqres.in/api/articles', article, { headers })
+//      .then(response => this.setState({ articleId: response.data.id }));
+  console.log(email);
+  console.log(parola);
+  const headers = {
+    'security-token': 'test',
+  };
+  return axios.post("http://meetprep.beta.bitstone.eu/api/v1/login", {
+    email : email,
+    password : parola
+  }, {headers} );
+}
